f9960405d5617f86e56a4eb11b4f18631722672b
terryyin <terry@odd-e.com>
2013-09-01 11:02:28 +0800

    very first test passed

--- py_book/__init__.py
+++ py_book/__init__.py
@@ -1,7 +1,15 @@
 '''
 To be added
 '''
+
 VERSION = "0.0.2"
 
+import markdown
+
+def toHTML(pathname):
+    ''' Convert a folder or a md file into HTML.
+    '''
+    with open(pathname, 'r') as f:
+        return markdown.markdown(f.read())
 
 
--- tests/folder_as_markdown.py
+++ tests/folder_as_markdown.py
@@ -1,9 +1,12 @@
 import unittest
+from mock import patch, MagicMock
+from py_book import toHTML
 
 
-class Test(unittest.TestCase):
-
-
+class TestConvertFileAndFolder(unittest.TestCase):
+    ''' pybook should be able to convert both md File and
+        folder into HTML.
+    '''
     def setUp(self):
         pass
 
@@ -12,6 +15,14 @@ class Test(unittest.TestCase):
         pass
 
 
-    def testName(self):
-        pass
+    @patch('py_book.open', create=True)
+    def test_convert_MD_file_into_html(self, mock_open):
+        mock_open.return_value = MagicMock(spec=file)
+        file_handle = mock_open.return_value.__enter__.return_value
+        file_handle.read.return_value = "#ABC"
+        
+        c = toHTML("/path/to/markdown.md")
 
+        mock_open.assert_called_with("/path/to/markdown.md", 'r')
+        self.assertEqual("<h1>ABC</h1>", c)
+        
