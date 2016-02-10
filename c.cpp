c7733223d11af60fe5a117b3f3a7fad4322361ea
Terry Yin <terry@odd-e.com>
2016-02-10 11:24:33 +0800

    try

--- c.cpp
+++ c.cpp
@@ -1,2 +1,2 @@
-void f() {}
+void f(){}
 
--- testlizard.py
+++ testlizard.py
@@ -1,7 +1,21 @@
-from lizard import analyze, preprocessing
+from lizard import analyze, preprocessing, CLikeReader, FileInfoBuilder
 
 r = analyze(["c.cpp"], None, 1, [preprocessing])
 for i in r:
     print "-----file--------"
     print i.filename
     print i.function_list
+
+def analyze_source_code(filename):
+    context = FileInfoBuilder(filename)
+    reader = CLikeReader(context)
+    with open(filename) as f:
+        tokens = reader.generate_tokens(f.read())
+    for _ in reader(tokens, reader):
+        pass
+    return context.fileinfo
+
+
+i = analyze_source_code("c.cpp")
+print i.filename
+print i.function_list
